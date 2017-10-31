f5fcf26c07aa2f6d019bfc1b5a801dd90a13b7c0
Thirumani Raj <thirumaniraj3@gmail.com>
2017-10-31 11:44:35 +0530

    Add files via upload

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,4 +1,19 @@
+self.addEventListener('beforeinstallprompt', function(e) {
+  // beforeinstallprompt Event fired
 
+  // e.userChoice will return a Promise.
+  e.userChoice.then(function(choiceResult) {
+
+    console.log(choiceResult.outcome);
+
+    if(choiceResult.outcome == 'dismissed') {
+      console.log('User cancelled home screen install');
+    }
+    else {
+      console.log('User added to home screen');
+    }
+  });
+});
 
 
 //This is the "Offline copy of pages" wervice worker
